47c2b030950afb3355dbff2f5b56dc49b5b0cf63
Edwin Mghendi <ediiethan16gmail.com>
2025-09-26 13:40:52 +0300

    used a function to choose a random number

--- projects.js
+++ projects.js
@@ -2,3 +2,12 @@
 const arrQuestion=[{questions:"",answer:""},
     {questions:"",answer:""}
 ];
+const quest =findQ(arrQuestion);
+console.log();
+function findQ(array){
+    for(i=0;i<array.length;i++){
+        let random=Math.floor(Math.random()*array.length);
+        let selectedQ=array[random];
+        return selectedQ;
+    }
+}
