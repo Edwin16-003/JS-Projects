fa1132d357677ef14535b474d6ebd0458ad1d776
Edwin Mghendi <ediiethan16gmail.com>
2025-09-26 13:53:37 +0300

    inserted some comments in between the code

--- projects.js
+++ projects.js
@@ -2,12 +2,18 @@
 const arrQuestion=[{questions:"",answer:""},
     {questions:"",answer:""}
 ];
+//call a function to get a random question
 const quest =findQ(arrQuestion);
+//display the question to the console
 console.log("The question is:",quest.questions);
+//use the function to randomly select a question
 function findQ(array){
     for(i=0;i<array.length;i++){
         let random=Math.floor(Math.random()*array.length);
         let selectedQ=array[random];
         return selectedQ;
     }
+}
+function findAnswer(){
+
 }
